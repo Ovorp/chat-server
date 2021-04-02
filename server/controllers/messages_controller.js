4a65d148f8d269df53e458d612769f578f8523bc
Nathaniel Shirts <nshirts7@gmail.com>
2021-04-01 19:59:36 -0600

    finished lab

---
+++ server/controllers/messages_controller.js
@@ -0,0 +1,42 @@
+const messages = [];
+let id = 0;
+
+// a message will be {id, text, time }
+
+create = (req, res) => {
+  const { text, time } = req.body;
+  messages.push({ text, time, id });
+  id++;
+  res.status(200).json(messages);
+};
+
+read = (req, res) => {
+  res.status(200).json(messages);
+};
+
+update = (req, res) => {
+  const { text, time } = req.body;
+  const { id } = req.params;
+  messages.forEach((val, i) => {
+    if (val.id === +id) {
+      messages[i] = {
+        text: text || messages[i].text,
+        time: time || messages[i].time,
+        id: +id,
+      };
+    }
+  });
+  res.status(200).json(messages);
+};
+
+toDelete = (req, res) => {
+  const { id } = req.params;
+  messages.forEach((val, i) => {
+    if (val.id === +id) {
+      messages.splice(i, 1);
+    }
+  });
+  res.status(200).json(messages);
+};
+
+module.exports = { create, read, update, toDelete };
---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const {
+  read,
+  update,
+  create,
+  toDelete,
+} = require('./controllers/messages_controller');
+const PORT = 3001;
+app.use(express.static('/../public/build'));
+app.use(express.json());
+
+app.get('/api/messages', read);
+app.post('/api/messages', create);
+app.put('/api/messages/:id', update);
+app.delete('/api/messages/:id', toDelete);
+
+app.listen(PORT, console.log(`Hello World on Port ${PORT}`));
