56844994a99cc2fc4f6b482d2a7cb6aa30a7c144
Nathaniel Shirts <nshirts7@gmail.com>
2021-04-02 20:13:41 -0600

    added comments about endpoints

--- server/index.js
+++ server/index.js
@@ -10,9 +10,16 @@ const PORT = 3001;
 app.use(express.static(__dirname + '/../public/build'));
 app.use(express.json());
 
+// get endpoints
 app.get('/api/messages', read);
+
+// post endpoints
 app.post('/api/messages', create);
+
+// put endpoints
 app.put('/api/messages/:id', update);
+
+// delete endpoints
 app.delete('/api/messages/:id', toDelete);
 
 app.listen(PORT, console.log(`Hello World on Port ${PORT}`));
